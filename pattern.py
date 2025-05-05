ba147ed28493ad75298b65211c32417808a757fc
Z-code957 <pallavisethupadhyay@gmail.com>
2025-05-05 17:23:58 +0800

    Add files via upload

--- pattern.py
+++ pattern.py
@@ -1,6 +1,13 @@
-print("creating half pyramid pattern")
+"""print("creating half pyramid pattern")
 n = int(input("Enter the number of rows: "))
 for i in range(n):
     for j in range(i+1):
         print("*",end="")
+    print()"""
+n = int(input("Enter the number of rows: "))
+number = 1
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(number,end=" ")
+        number = number+1
     print()
